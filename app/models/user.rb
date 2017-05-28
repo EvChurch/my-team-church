b9f04356767ff50f8e3e8b71221dd2a0870d1929
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-05-28 17:17:42 +1200

    remove elvanto omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:elvanto]
+         :recoverable, :rememberable, :trackable, :validatable
+         # :omniauthable, omniauth_providers: [:elvanto]
   enum role: %i[user vip admin]
   after_initialize :set_default_role, if: :new_record?
   has_one :person, foreign_key: :username, primary_key: :username
