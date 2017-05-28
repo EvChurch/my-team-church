b46d3c0ceb081f35387482c45eeb4c1ddce6cf2c
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-05-28 15:51:52 +1200

    class fixes

--- app/policies/position_policy.rb
+++ app/policies/position_policy.rb
@@ -1,14 +1,10 @@
-class Department
-  class SubDepartment
-    class PositionPolicy < ApplicationPolicy
-      class Scope < Scope
-        def resolve
-          if person.admin?
-            scope.all
-          else
-            person.positions.eager_load(:people)
-          end
-        end
+class PositionPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if person.admin?
+        scope.all
+      else
+        person.positions.eager_load(:people)
       end
     end
   end
--- app/policies/sub_department_policy.rb
+++ app/policies/sub_department_policy.rb
@@ -1,19 +1,17 @@
-class Department
-  class SubDepartmentPolicy < ApplicationPolicy
-    class Scope < Scope
-      def resolve
-        if person.admin?
-          scope.eager_load(
+class SubDepartmentPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if person.admin?
+        scope.eager_load(
+        :positions
+        ).order('department_sub_departments.name, department_sub_department_positions.name')
+      else
+        person.sub_departments.eager_load(
           :positions
-          ).order('department_sub_departments.name, department_sub_department_positions.name')
-        else
-          person.sub_departments.eager_load(
-            :positions
-          ).where(
-            'positions_department_sub_departments.id' => person.position_ids
-          ).distinct.order('department_sub_departments.name, positions_department_sub_departments.name')
+        ).where(
+          'positions_department_sub_departments.id' => person.position_ids
+        ).distinct.order('department_sub_departments.name, positions_department_sub_departments.name')
 
-        end
       end
     end
   end
