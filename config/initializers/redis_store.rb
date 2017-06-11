c8a0e161d2930e44f2b46a0de37ec39c54652a07
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-06-12 00:57:12 +1200

    syntax error

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
-Rails.config.cache_store = :redis_store, ENV.fetch('REDIS_URL')
-Rails.config.session_store :redis_store,
+Rails.application.config.cache_store = :redis_store, ENV.fetch('REDIS_URL')
+Rails.application.config.session_store :redis_store,
                            redis_server: ENV.fetch('REDIS_URL'),
                            domain: ".#{ENV.fetch('DOMAIN_NAME')}",
                            expires_in: 2.days,
