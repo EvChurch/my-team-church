deb8640367b4816c4dd2e646849918a28700d1ad
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-02-05 09:39:39 +1300

    update application

--- config/application.rb
+++ config/application.rb
@@ -8,30 +8,32 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-class MyPlace::Application < Rails::Application
-  config.generators do |g|
-    g.test_framework :rspec,
-                     fixtures: true,
-                     view_specs: false,
-                     helper_specs: false,
-                     routing_specs: false,
-                     controller_specs: false,
-                     request_specs: false
-    g.fixture_replacement :factory_girl, dir: 'spec/factories'
-    g.orm :active_record, primary_key_type: :uuid
-  end
+module MyPlace
+  class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.orm :active_record, primary_key_type: :uuid
+    end
 
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-  config.autoload_paths += Dir["#{config.root}/lib/"]
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += Dir["#{config.root}/lib/"]
 
-  config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_adapter = :sidekiq
 
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource '*', headers: :any, methods: %i[post options]
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[post options]
+      end
     end
   end
 end
