9be63e82ce09a75766ed668acbf15799e25a7947
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-02-05 17:20:34 +1300

    title fixes

--- app/javascript/app/app.run.js
+++ app/javascript/app/app.run.js
@@ -1,4 +1,13 @@
 /* @ngInject*/
 export default function appRun(
+  $rootScope, $transitions, $window
 ) {
+  $transitions.onFinish(null, (trans) => {
+    changePageTitle(trans, $rootScope, $window);
+  });
+}
+
+function changePageTitle(transition, $rootScope, $window) {
+    const newState = transition.$to();
+    $rootScope.pageTitle = newState.title;
 }
