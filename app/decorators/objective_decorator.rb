8dfba3ce2d694c0efc559db4bbef49fcf0673c7d
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-04-29 16:34:44 +1200

    #58 objective percentage should only use kept

--- app/decorators/objective_decorator.rb
+++ app/decorators/objective_decorator.rb
@@ -4,6 +4,7 @@ class ObjectiveDecorator < ApplicationDecorator
   decorates_association :key_results
 
   def percentage_complete
-    key_results.map(&:progress_percentage).sum.fdiv(object.key_results.sum(:weight)) * 100
+    key_results = object.key_results.kept.map { |key_result| key_result.decorate.progress_percentage }
+    key_results.sum.fdiv(object.key_results.kept.sum(:weight)) * 100
   end
 end
